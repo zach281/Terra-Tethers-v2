cf3e1b5404f3de8dc1318417ad7e6ba429c6279a
zach281 <zachary.senior@gmail.com>
2026-04-28 20:37:44 -0600

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -11,7 +11,9 @@ export async function proxy(request: NextRequest) {
       cookies: {
         getAll() { return request.cookies.getAll() },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value)
+          )
           supabaseResponse = NextResponse.next({ request })
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set(name, value, options)
@@ -22,7 +24,6 @@ export async function proxy(request: NextRequest) {
   )
 
   await supabase.auth.getUser()
-
   return supabaseResponse
 }
 
